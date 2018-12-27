775e7c26157b9c35a015af67fd23677631cae161
Gulshan Kumar <inbox.gulshan@outlook.com>
2018-12-27 22:03:27 +0530

    Showing How to use directives in courses.component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+            <h2>{{ getTitle() }}</h2>
+            <ul>
+                <li *ngFor="let course of courses">{{ course }}</li>
+            </ul>
+        `
 })
 export class CoursesComponenet{
+    title = "List of Courses";
+    courses = ["Course 1", "Course 2", "Course 3"];
 
+    getTitle(){
+        return this.title;
+    }
 }
