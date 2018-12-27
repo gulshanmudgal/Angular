af33483c2493792e158cf001d023a5c987c3e681
Gulshan Kumar <inbox.gulshan@outlook.com>
2018-12-27 21:41:34 +0530

    Registered Courses Component in App component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponenet } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponenet
   ],
   imports: [
     BrowserModule,
